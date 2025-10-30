2874d02b1f17a165ba71f3baecd03ff09e7a771d
Aminjoundi <joundiamin65gmail.com>
2025-10-30 11:18:45 +0100

    second commit

--- defi9.js
+++ defi9.js
@@ -1,1 +1,11 @@
 const prompt = require("prompt-sync")();
+
+let x=parseInt(prompt("Entrer le nombre de bouteilles : "))
+let  b=0,v=0;
+while (x>0) {
+ b= b+x;
+ v=v+x;
+ x=Math.floor(v/3)
+ v=v%3;
+}
+console.log(b)
